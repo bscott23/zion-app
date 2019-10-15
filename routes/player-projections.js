be660ee958cf190442a4bb1cae1131f9b7e74e5b
board-man <brodyscott12@gmail.com>
2019-10-14 20:54:16 -0400

    Added sample-json.js file to use as data proxy until backend server is complete

---
+++ routes/player-projections.js
@@ -0,0 +1,5 @@
+// const router = require('express').Router();
+const data = require('../data/sample-json');
+
+// this will be updated to pull projections data from a server
+// in the meantime, data will be imported to directly to the client like above
